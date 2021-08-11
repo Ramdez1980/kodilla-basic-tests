4bfd2fa2f4067cf5cfc2122637baeeaa460d4086
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-08-12 00:12:57 +0200

    KOD-3.5

---
+++ src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,35 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int numberA = 5;
+        int numberB = 8;
+        int numberC = 9;
+        int numberD = 2;
+        int sumResult = calculator.adingNumber(numberA, numberB);
+        int subResult = calculator.substractNumber(numberA, numberB); // zadanie domowe 3.5.1//
+        int powerResult = calculator.power(numberC, numberD);
+
+        boolean correct = ResultChecker.assertEquals(13, sumResult);
+        if (correct) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + numberA + " i " + numberB);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + numberA + " i " + numberB);
+        }
+
+        boolean correctSubstract = ResultChecker.assertEquals(-3, subResult);                // zadanie domowe 3.5.1//
+        if (correctSubstract) {
+            System.out.println("Metoda sum działa poprawnie dla liczb " + numberA + " i " + numberB); // zadanie domowe 3.5.1//
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + numberA + " i " + numberB); // zadanie domowe 3.5.1//
+        }
+
+        boolean correctPower = ResultChecker.assertEquals(81,powerResult);
+        if (correctPower) {
+            System.out.println("Metoda potegowania do kwadratu  działa poprawnie dla liczb " + numberC + " i " + numberD);
+        } else {
+            System.out.println("Metoda potegowania do kwadratu  nie działa poprawnie dla liczb " + numberC + " i " + numberD);
+        }
+    }
+}
---
+++ src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,27 @@
+package com.kodilla.basic_assertion;
+
+
+public class Calculator {
+
+    int result;
+
+    int adingNumber(int numberA, int numberB)
+    {
+        return  numberA + numberB;
+    }
+
+    int substractNumber(int numberA, int numberB)
+    {
+        return numberA - numberB;
+
+    }
+
+    int power(int numberC, int numberD)                              //zadanie domowe 3.5.2//
+    {
+       return (int) Math.pow(numberC, numberD);                     //zadanie domowe 3.5.2//
+    }
+
+
+
+
+}
