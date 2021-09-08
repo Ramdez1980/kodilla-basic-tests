21c98de636aa581e1b3d7aa916908d68cce053f5
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-09-08 23:57:21 +0200

    KOD-5.3

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -1,12 +1,20 @@
 package com.kodilla.collections.adv.immutable.homework;
 
-public class Task {
+public final class Task {
 
-    protected final String title;
-    protected final int duration;
+    private final String title;
+    private final int duration;
 
     public Task(String title, int duration) {
         this.title = title;
         this.duration = duration;
+    }
+
+    public final String getTitle() {
+        return title;
+    }
+
+    public final int getDuration() {
+        return duration;
     }
 }
