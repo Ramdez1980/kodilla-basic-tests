a878af7645a058e523d6bc9e5be3cd2441d9063d
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-09-08 23:39:49 +0200

    KOD-6.1 Task

--- kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskManager.java
@@ -4,16 +4,13 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 public class TaskManager {
+
     public static void main(String[] args) {
-        List<String> tasks = TaskRepository.getTasks()
-            .stream()
-            .filter(u -> u.getDeadline().isAfter(LocalDate.now()))
-            .map(TaskManager::getDeadLine)
-            .collect(Collectors.toList());
-            System.out.println(tasks);
+        List <LocalDate> deadlines = TaskRepository.getTasks()
+                .stream()
+                .filter(task -> task.getDeadline().isAfter(LocalDate.now()))
+                .map(task -> task.getDeadline())
+                .collect(Collectors.toList());
     }
 
-    public static String getDeadLine(Task task) {
-        return task.getName() + " " + task.getDeadline() ;
-    }
 }
