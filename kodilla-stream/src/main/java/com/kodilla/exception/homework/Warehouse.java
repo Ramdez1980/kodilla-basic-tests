0517f4498c7b1d5f0387c31d27ab3e3d5d3b43c4
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-09-20 20:09:30 +0200

    KOD-6.4

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -1,6 +1,27 @@
 package com.kodilla.exception.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Warehouse {
+    public static List<Order> addOrder() {
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order("1/2021"));
+        orders.add(new Order("2/2021"));
+        orders.add(new Order("3/2021"));
+        orders.add(new Order("4/2021"));
+        orders.add(new Order("5/2021"));
+
+        return orders;
+    }
+
+    public static Order getOrder(String number) {
+        return getOrder("6/2021");
+    }
+
+
+
+
 
 
 }
--- kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -1,7 +1,17 @@
 package com.kodilla.exception.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class WarehouseApp {
     public static void main(String[] args) {
 
+
+        List<Order> orders = new ArrayList<>();
+        for (Order order : Warehouse.addOrder()) {
+            String number = order.getNumber();
+            orders.add(order);
+        }
     }
 }
