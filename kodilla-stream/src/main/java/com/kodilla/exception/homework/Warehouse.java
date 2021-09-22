678ad397c3803b3b07279832f00c40d9a88b1018
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-09-22 21:16:18 +0200

    KOD-6.4

--- kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/Warehouse.java
@@ -1,21 +1,21 @@
 package com.kodilla.exception.homework;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Warehouse {
-    public static List<Order> addOrder() {
-        List<Order> orders = new ArrayList<>();
-        orders.add(new Order("1/2021"));
-        orders.add(new Order("2/2021"));
-        orders.add(new Order("3/2021"));
-        orders.add(new Order("4/2021"));
-        orders.add(new Order("5/2021"));
 
-        return orders;
+    public List<Order> getOrders (String number) {
+        return new ArrayList<>();
     }
 
-    public static Order getOrder(String number) {
-        return getOrder("6/2021");
+
+
+    public List<Order> addOrder(Order order) {
+        return addOrder(new Order(""));
     }
+
+
 }
--- kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
+++ kodilla-stream/src/main/java/com/kodilla/exception/homework/WarehouseApp.java
@@ -2,17 +2,21 @@ package com.kodilla.exception.homework;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WarehouseApp {
     public static void main(String[] args) {
+            ArrayList<Order> orders  = new ArrayList<>();
+            orders.add(new Order("1/2021"));
+            orders.add(new Order("2/2021"));
+            orders.add(new Order("3/2021"));
+            orders.add(new Order("4/2021"));
+            orders.add(new Order("5/2021"));
 
-        List<Order> orders = new ArrayList<>();
-        for (Order order : Warehouse.addOrder()) {
+
+
+        for (Order order :  ) {
             String number = order.getNumber();
             orders.add(order);
         }
-
-
     }
 }
