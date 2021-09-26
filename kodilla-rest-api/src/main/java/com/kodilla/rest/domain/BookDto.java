e0068c30b77cdfebf8111ef7be1e9068ebdbecf7
Ramdez1980 <53618557+Ramdez1980@users.noreply.github.com>
2021-09-26 17:45:44 +0200

    Kod 10.3 (#16)
    * KOD-10.3 part 1
    
    * KOD-10.3 first
    
    * KOD-10.3 first

---
+++ kodilla-rest-api/src/main/java/com/kodilla/rest/domain/BookDto.java
@@ -0,0 +1,36 @@
+package com.kodilla.rest.domain;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+public class BookDto {
+    private String title;
+    private String author;
+
+    public BookDto(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() { return title; }
+
+    public String getAuthor() { return author; }
+
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BookDto)) return false;
+        BookDto bookDto = (BookDto) o;
+        return getTitle().equals(bookDto.getTitle()) && getAuthor().equals(bookDto.getAuthor());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getAuthor());
+    }
+
+
+}
