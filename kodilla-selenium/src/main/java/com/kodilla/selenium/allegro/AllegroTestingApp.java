6dcb97ed190d2d849a7c4d44650d3f6c629cadf2
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-10-04 00:14:17 +0200

    KOD-13.4

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -1,21 +1,20 @@
-package com.kodilla.selenium.google;
+package com.kodilla.selenium.allegro;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class GoogleExample {
+public class AllegroTestingApp {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get("https://www.allegro.pl/");
         Thread.sleep(5000);
-        WebElement inputField = driver.findElement(By.name("q"));
-        inputField.sendKeys("Kodilla");
-        inputField.submit();
+        WebElement acceptCookies = driver.findElement(By.xpath("//html/body/div/div/header/div/div/div/form/div/select/optgroup/option[3]"));
+
+
+
     }
 }
