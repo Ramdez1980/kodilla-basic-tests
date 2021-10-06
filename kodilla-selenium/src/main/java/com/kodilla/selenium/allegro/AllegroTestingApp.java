c21f0593e7e8ebaacf1b14f734555c8920649541
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-10-06 14:46:26 +0200

    KOD-13.4

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -5,13 +5,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class AllegroTestingApp {
+
+
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.allegro.pl/");
-        Thread.sleep(5000);
-        WebElement findField = driver.findElement(By.xpath("//html/body/div/div/header/div/div/div/form/div/select/optgroup/[option value = \"/kategoria/elektronika\"]"));
-        findField.submit();
+        Thread.sleep(2000);
+
+        WebElement acceptCookies = driver.findElement(By.xpath("//html/body/div[3]/div[8]/div/div/div/div/div[2]/div[2]/button[1]"));
+        acceptCookies.click();
+
+
+        WebElement inputField = driver.findElement(By.xpath("//*[@data-box-name=\"allegro.metrumHeader.search\"]/div/form/div[3]/div/select"));
+        inputField.sendKeys("Elektronika");
+        inputField = driver.findElement(By.xpath("//*[@data-box-name=\"allegro.metrumHeader.search\"]/div/form/input"));
+        inputField.sendKeys("Mavic mini");
+        inputField = driver.findElement(By.xpath("//*[@data-box-name=\"allegro.metrumHeader.search\"]/div/form/button"));
+        inputField.click();
     }
 }
