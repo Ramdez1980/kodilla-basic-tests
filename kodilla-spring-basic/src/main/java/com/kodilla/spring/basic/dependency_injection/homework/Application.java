0c6f2f21557df11fb1b1de80646dc8602097caa3
Ramdez1980 <53618557+Ramdez1980@users.noreply.github.com>
2021-10-06 21:17:31 +0200

    Kod 9.1 (#18)
    * KOD-9.1
    
    * KOD-9.1 v1
    
    * KOD-9.1 v2
    
    * KOD-9.1 final

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/dependency_injection/homework/Application.java
@@ -0,0 +1,9 @@
+package com.kodilla.spring.basic.dependency_injection.homework;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ShippingCenter shippingCenter = new ShippingCenter(new DhlDeliveryService(), new DhlNotificationService());
+        shippingCenter.sendPackage("Warsaw", 15);
+    }
+}
