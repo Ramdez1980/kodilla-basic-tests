58ea20fc28934234c8630adef72a60690ff8a73a
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-10-10 18:02:48 +0200

    KOD-9.2.1

---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+public class Calculator {
+
+    private Display display;
+
+    public Calculator(Display display) {
+        this.display = display;
+    }
+
+
+
+
+}
---
+++ kodilla-spring-basic/src/main/java/com/kodilla/spring/basic/spring_dependency_injection/homework/Display.java
@@ -0,0 +1,8 @@
+package com.kodilla.spring.basic.spring_dependency_injection.homework;
+
+public class Display {
+
+    public String display(double value) {
+        return "Value [" + value + "]" ;
+    }
+}
