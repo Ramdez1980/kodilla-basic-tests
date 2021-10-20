431d0d7689d9c8845d3feed79af5026619194eac
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-10-20 21:26:33 +0200

    KOD-7.2

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -3,28 +3,37 @@ package com.kodilla.parametrized_tests.homework;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GamblingMachineTestSuite {
-    GamblingMachineTestSuite randomNumbers = new GamblingMachineTestSuite();
-    /*private boolean InvalidNumbersException;
-    private boolean howManyWins;*/
+
+    private GamblingMachine gamblingMachine = new GamblingMachine();
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/sixRandomNumbersFromFourtyNineFalse.csv", numLinesToSkip = 1)
-    public void shouldReverseFalseTest(int Number, int RandomNumber) {
-       assertFalse(randomNumbers.);
+    @CsvFileSource(resources = "/sixRandomNumbersFromFourtyNineFalse.csv", delimiter = ';')
+    public void shouldThrowException(String numbers) {
+        Set<Integer> nums = parse(numbers);
+        assertThrows(InvalidNumbersException.class, () -> gamblingMachine.howManyWins(nums));
     }
 
     @ParameterizedTest
-    @CsvFileSource(resources = "/sixRandomNumbersFromFourtyNineTrue.csv", numLinesToSkip = 1)
-    public void shouldReverseTrueTest(int Number, int RandomNumber) {
-        if(   Number >= 0 && Number <=6 )
-            assertTrue();
-        }
-
+    @CsvFileSource(resources = "/sixRandomNumbersFromFourtyNineTrue.csv", delimiter = ';')
+    public void shouldReturnNumber(String numbers) throws InvalidNumbersException {
+        Set<Integer> nums = parse(numbers);
+        int result = gamblingMachine.howManyWins(nums);
+        assertTrue(result >= 0 && result <=6);
     }
 
+    private static Set<Integer> parse(String str) {
+        return Arrays.stream(str.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toSet());
+    }
 }
+
 
 
