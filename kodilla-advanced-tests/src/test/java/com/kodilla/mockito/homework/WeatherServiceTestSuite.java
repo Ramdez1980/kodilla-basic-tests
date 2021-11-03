f9c68d8677d01a6813b479967cc06f2f2ca71de5
Jan Nowakowski <jan.nowakowski@poczta.onet.pl>
2021-11-03 21:20:00 +0100

    KOD-7.6

--- kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherServiceTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/mockito/homework/WeatherServiceTestSuite.java
@@ -5,6 +5,8 @@ import com.kodilla.notification.homework.Location;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.HashSet;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -53,93 +55,82 @@ public class WeatherServiceTestSuite {
         // given
         Subscriber subscriber1 = Mockito.mock(Subscriber.class);
         Subscriber subscriber2 = Mockito.mock(Subscriber.class);
-        Location location = Mockito.mock(Location.class);
         Location location1 = Mockito.mock(Location.class);
-        Set<Location> expected = new HashSet<>();
-        expected.add(location1);
+        Location location2 = Mockito.mock(Location.class);
+        weatherService.addSubscriberToLocations(subscriber1, location1);
+        weatherService.addSubscriberToLocations(subscriber1, location2);
+        weatherService.addSubscriberToLocations(subscriber2, location1);
+        weatherService.addSubscriberToLocations(subscriber2, location2);
+
+        Set<Subscriber> subscribers = new HashSet<>();
+        subscribers.add(subscriber2);
+        Map<Location, Set<Subscriber>> expected = new HashMap<>();
+        expected.put(location1, subscribers);
+        expected.put(location2, subscribers);
 
         // when
-        weatherService.removeLocation(location);
+        weatherService.removeAllLocations(subscriber1);
 
         // then
-        assertEquals(expected,weatherService.getLocationSubscribers().get(location));
+        assertEquals(expected, weatherService.getLocationSubscribers());
     }
 
     @Test
     public void should_sendWeatherNotification() {
+        // Given
         Location location = Mockito.mock(Location.class);
-        Location location2 = Mockito.mock(Location.class);
-        Location location3 = Mockito.mock(Location.class);
+        WeatherNotification weatherNotification = Mockito.mock(WeatherNotification.class);
         Subscriber subscriber = Mockito.mock(Subscriber.class);
-        Subscriber subscriber2 = Mockito.mock(Subscriber.class);
-        Subscriber subscriber3 = Mockito.mock(Subscriber.class);
 
         weatherService.addSubscriberToLocations(subscriber, location);
-        weatherService.addSubscriberToLocations(subscriber2,location);
-        weatherService.addSubscriberToLocations(subscriber3,location);
-        WeatherNotification weatherNotification = Mockito.mock(WeatherNotification.class);
 
+        // When
+        weatherService.sendWeatherNotification(weatherNotification, location);
 
-        weatherService.sendWeatherNotification(weatherNotification,location);
-        Mockito.verify(location, Mockito.times(55)).receiveWeather(weatherNotification);
-
+        // Then
+        Mockito.verify(subscriber).send(weatherNotification);
     }
 
+    @Test
+    public void should_sendToAll() {
+        // Given
+        WeatherNotification weatherNotification = Mockito.mock(WeatherNotification.class);
+        Location location1 = Mockito.mock(Location.class);
+        Location location2 = Mockito.mock(Location.class);
+        Subscriber subscriber1 = Mockito.mock(Subscriber.class);
+        Subscriber subscriber2 = Mockito.mock(Subscriber.class);
 
+        weatherService.addSubscriberToLocations(subscriber1, location1);
+        weatherService.addSubscriberToLocations(subscriber2, location1);
+        weatherService.addSubscriberToLocations(subscriber2, location2);
 
+        // When
+        weatherService.sendToAll(weatherNotification);
 
+        // Then
+        Mockito.verify(subscriber1, Mockito.times(1)).send(weatherNotification);
+        Mockito.verify(subscriber2, Mockito.times(1)).send(weatherNotification);
+    }
 
+    @Test
+    public void should_removeLocation() {
+        // Given
+        Location location1 = Mockito.mock(Location.class);
+        Location location2 = Mockito.mock(Location.class);
+        Subscriber subscriber = Mockito.mock(Subscriber.class);
 
+        weatherService.addSubscriberToLocations(subscriber, location1);
+        weatherService.addSubscriberToLocations(subscriber, location2);
 
-//
-//    @Test
-//    public void noSubscreiberShouldNotReceiveWeatherNotification() {
-//        weatherService.sendWeatherNotification(weatherNotification,location);
-//        Mockito.verify(location, Mockito.never()).receiveWeather(weatherNotification);
-//    }
-//
-//    @Test
-//    public void subscribedClientShouldReceiveWeatherNotification() {
-//        weatherService.addSubscriberToLocations(subscriber, location);
-//        weatherService.sendWeatherNotification(weatherNotification,location);
-//     /*Mockito.verify(subscriber, Mockito.times(1)).receive(weatherNotification);*/
-//    }
-//
-//    @Test
-//    public void notificationShouldBeSentToAllSubscribedWeatherClients() {
-//        Subscriber secondSubscriber = Mockito.mock(Subscriber.class);
-//        Subscriber thirdSubscriber = Mockito.mock(Subscriber.class);
-//        Location secondLocation = Mockito.mock(Location.class);
-//        Location thirdLocation = Mockito.mock(Location.class);
-//        WeatherNotification secondWeatherNotification = Mockito.mock(WeatherNotification.class);
-//        WeatherNotification thirdWeatherNotification = Mockito.mock(WeatherNotification.class);
-//        weatherService.addSubscriberToLocations(subscriber,location);
-//        weatherService.addSubscriberToLocations(secondSubscriber,secondLocation);
-//        weatherService.addSubscriberToLocations(thirdSubscriber,thirdLocation);
-//
-//        weatherService.sendWeatherNotification(weatherNotification,location);;
-//    }
-//
-//    @Test
-//    public void shouldSendOnlyOneNotificationToMultiTimeSubscriber() {
-//        Location secondLocation = Mockito.mock(Location.class);
-//        Location thirdLocation = Mockito.mock(Location.class);
-//        Subscriber secondSubscriber = Mockito.mock(Subscriber.class);
-//        Subscriber thirdSubscriber = Mockito.mock(Subscriber.class);
-//
-//        weatherService.addSubscriberToLocations(subscriber,location);
-//        weatherService.addSubscriberToLocations(secondSubscriber,location);
-//        weatherService.addSubscriberToLocations(thirdSubscriber,secondLocation);
-//
-//        weatherService.sendWeatherNotification(weatherNotification,location);
-//    }
-//
-//    @Test
-//    public void unsubscribedClientShouldNotReceiveNotification() {
-//        weatherService.addSubscriberToLocations(subscriber,location);
-//        weatherService.removeWeatherSubscriber(subscriber,location);
-//
-//        weatherService.sendWeatherNotification(weatherNotification,location);
-//        Mockito.verify(subscriber, Mockito.never()).receive(weatherNotification);
-//    }
+        Set<Subscriber> subscribers = new HashSet<>();
+        subscribers.add(subscriber);
+        Map<Location, Set<Subscriber>> expected = new HashMap<>();
+        expected.put(location2, subscribers);
+
+        // When
+        weatherService.removeLocation(location1);
+
+        // Then
+        assertEquals(expected, weatherService.getLocationSubscribers());
+    }
 }
